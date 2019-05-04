6bdb39612e56f96c57271b2c6eab5ae37f806f62
Rohan Agrawal <send2arohan@gmail.com>
2019-05-04 07:41:54 -0700

    Add docker support and supporting config

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM buildbot/buildbot-master:v2.2.0
+
+RUN pip3 install boto3
+
+WORKDIR /var/lib/buildbot
+
+COPY . .
+
+# Setup volumes for building and credentials
+VOLUME /buildwork
+VOLUME /creds
+
+CMD ["dumb-init", "/usr/src/buildbot/docker/start_buildbot.sh"]
+
---
+++ creds/fake_creds.py
@@ -0,0 +1,20 @@
+hydrogen = "fake"
+helium = "fake"
+lithium = "fake"
+beryllium = "fake"
+boron = "fake"
+
+dasher = "fake"
+dancer = "fake"
+prancer = "fake"
+
+stmp = "fake"
+
+GithubId = "fake"
+GithubSecret = "fake"
+
+awsPub = "fake"
+awsPriv = "fake"
+
+aptly = "buildbot:fake"
+
