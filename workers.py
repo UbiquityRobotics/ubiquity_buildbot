d3abd65c05f1efea496ff5badc2e7838d42972a0
Rohan Agrawal <send2arohan@gmail.com>
2019-05-04 16:01:25 -0700

    Reduce noise by splitting out workers config

---
+++ workers.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+from get_creds import creds
+
+from buildbot.plugins import *
+
+workers = []
+
+# Legacy ARM Builders
+workers.append(worker.Worker("hydrogen", creds.hydrogen, max_builds=1))
+workers.append(worker.Worker("helium", creds.helium, max_builds=1))
+workers.append(worker.Worker("lithium", creds.lithium, max_builds=1))
+
+# Legacy Intel Builders
+workers.append(worker.Worker("dasher", creds.dasher))
+workers.append(worker.Worker("dancer", creds.dancer))
+
+## AWS based ARM builders
+workers.append(worker.EC2LatentWorker("boron", creds.boron, 'a1.medium', 
+                    region="us-east-2",
+                    ami="ami-0a943ada6c1b11979", 
+                    identifier=creds.awsPub, 
+                    secret_identifier=creds.awsPriv, 
+                    keypair_name='awsBuildbots', 
+                    security_name="awsBuildbots", 
+                    spot_instance=True,
+                    max_spot_price=0.015,
+                    price_multiplier=None
+))
+
+
+# AWS Intel Builders
+workers.append(worker.EC2LatentWorker("prancer", creds.prancer, 'm5.large',
+                    region="us-east-2",
+                    ami="ami-00535e270cac192be",
+                    identifier=creds.awsPub,
+                    secret_identifier=creds.awsPriv,
+                    keypair_name='awsBuildbots',
+                    security_name="awsBuildbots",
+                    spot_instance=True,
+                    max_spot_price=0.028,
+                    price_multiplier=None
+))
+
+
+# AWS Windows Builder for firmware builds
+workers.append(worker.EC2LatentWorker("tofu", creds.tofu, 't3a.medium',
+                    region="us-east-2",
+                    ami="ami-0bd35b8c6065abdcb",
+                    identifier=creds.awsPub,
+                    secret_identifier=creds.awsPriv,
+                    spot_instance=True,
+                    keypair_name='awsBuildbots',
+                    security_name="awsBuildbots",
+                    max_spot_price=0.032,
+                    price_multiplier=None
+))
+
+
+armhf_workers = ["hydrogen", "helium", "lithium", "boron"]
+amd64_workers = ["dasher", "dancer", "prancer"]
+
+
