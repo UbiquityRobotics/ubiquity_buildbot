4b2dd966795e429608f5717f05504ae8ae3002dd
Rohan Agrawal <send2arohan@gmail.com>
2019-05-05 10:13:14 -0700

    Custom buildstep for aptly copy

---
+++ aptly_steps.py
@@ -0,0 +1,162 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+from twisted.internet import defer
+from twisted.internet import reactor
+
+from buildbot import config
+from buildbot.process.buildstep import FAILURE
+from buildbot.process.buildstep import SUCCESS
+from buildbot.process.buildstep import BuildStep
+
+import json
+
+# use the 'requests' lib: http://python-requests.org
+try:
+    import txrequests
+    import requests
+except ImportError:
+    txrequests = None
+
+
+# This step uses a global Session object, which encapsulates a thread pool as
+# well as state such as cookies and authentication.  This state may pose
+# problems for users, where one step may get a cookie that is subsequently used
+# by another step in a different build.
+
+_session = None
+
+
+def getSession():
+    global _session
+    if _session is None:
+        _session = txrequests.Session()
+        reactor.addSystemEventTrigger("before", "shutdown", closeSession)
+    return _session
+
+
+def setSession(session):
+    global _session
+    _session = session
+
+
+def closeSession():
+    global _session
+    if _session is not None:
+        _session.close()
+        _session = None
+
+class AptlyCopyPackageStep(BuildStep):
+
+    name = 'AptlyCopyPackageStep'
+    description = 'Copying Package'
+    descriptionDone = 'Copied Package'
+    renderables = ["aptly_base_url", "from_repo", "dest_repo", "package_query"]
+    session = None
+
+    def __init__(self, aptly_base_url, from_repo, dest_repo, package_query, **kwargs):
+        if txrequests is None:
+            config.error(
+                "Need to install txrequest to use this step:\n\n pip3 install txrequests")
+
+        self.aptly_base_url = aptly_base_url
+        self.from_repo = from_repo
+        self.dest_repo = dest_repo
+        self.package_query = package_query
+
+        super().__init__(**kwargs)
+
+    def start(self):
+        d = self.doCopy()
+        d.addErrback(self.failed)
+
+    @defer.inlineCallbacks
+    def doRequest(self, request_kwargs):
+        log.addHeader('Performing %s request to %s\n' %
+                      (request_kwargs.method, request_kwargs.url))
+        if request_kwargs.params:
+            log.addHeader('Parameters:\n')
+            for k, v in request_kwargs.params:
+                log.addHeader('\t%s: %s\n' % (k, v))
+
+        try:
+            r = yield self.session.request(**request_kwargs)
+        except requests.exceptions.ConnectionError as e:
+            log.addStderr(
+                'An exception occurred while performing the request: %s' % e)
+            self.finished(FAILURE)
+            return
+
+        if r.history:
+            log.addStdout('\nRedirected %d times:\n\n' % len(r.history))
+            for rr in r.history:
+                self.log_response(rr)
+                log.addStdout('=' * 60 + '\n')
+
+        self.log_response(r)
+
+        return r
+
+    @defer.inlineCallbacks
+    def doCopy(self):
+        # create a new session if it doesn't exist
+        self.session = getSession()
+        log = self.addLog('log')
+
+        search_kwargs = {
+            'method': 'GET',
+            'url': self.aptly_base_url + '/api/repos/' + self.from_repo + '/packages',
+            'params': {'q' : self.package_query}
+        }
+
+        r_search = yield doRequest(search_kwargs)
+        if (r_search.status_code >= 400):
+            self.finished(FAILURE)
+            log.finish()
+        
+        copy_kwargs = {
+            'method': 'POST',
+            'url': self.aptly_base_url + '/api/repos/' + self.dest_repo + '/packages',
+            'json': {'PackageRefs' : r_search.json()}  
+        }
+
+        r_copy = yield doRequest(copy_kwargs)
+        if (r_search.status_code >= 400):
+            self.finished(FAILURE)
+            log.finish()
+
+        self.finished(SUCCESS)
+
+    def log_response(self, response):
+        log = self.getLog('log')
+
+        log.addHeader('Request Header:\n')
+        for k, v in response.request.headers.items():
+            log.addHeader('\t%s: %s\n' % (k, v))
+
+        log.addStdout('URL: %s\n' % response.url)
+
+        if response.status_code == requests.codes.ok:
+            log.addStdout('Status: %s\n' % response.status_code)
+        else:
+            log.addStderr('Status: %s\n' % response.status_code)
+
+        log.addHeader('Response Header:\n')
+        for k, v in response.headers.items():
+            log.addHeader('\t%s: %s\n' % (k, v))
+
+        log.addStdout(' ------ Content ------\n%s' % response.text)
+        self.addLog('content').addStdout(response.text)
+
--- ubiquity_buildsteps.py
+++ ubiquity_buildsteps.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from get_creds import creds
+import re
 from buildbot.plugins import *
 
-import re
+from get_creds import creds
+import aptly_steps
 
 def set_deb_properties(name, factory, arch, is_ros=False):
     '''
@@ -22,11 +23,13 @@ def set_deb_properties(name, factory, arch, is_ros=False):
             value=util.Interpolate("/var/cache/pbuilder/result/xenial/ros-kinetic-" + debian_name + "_%(prop:deb_version)s_" + arch +".deb")))
         factory.addStep(steps.SetProperty(property="dsc_path", 
             value=util.Interpolate("../ros-kinetic-" + debian_name + "_%(prop:deb_version)s.dsc")))
+        factory.addStep(steps.SetProperty(property="deb_package_name", value='ros-kinetic-' + debian_name))
     else:
         factory.addStep(steps.SetProperty(property="deb_path", 
             value=util.Interpolate("/var/cache/pbuilder/result/xenial/" + debian_name + "_%(prop:deb_version)s_" + arch +".deb")))  
         factory.addStep(steps.SetProperty(property="dsc_path", 
             value=util.Interpolate("../" + debian_name + "_%(prop:deb_version)s.dsc")))
+        factory.addStep(steps.SetProperty(property="deb_package_name", value=debian_name))
 
 def cowbuilder_test_path(cow_config):
     '''
@@ -65,40 +68,6 @@ def cowbuilder(arch, factory):
     factory.addStep(steps.ShellCommand(command=['sudo', 'cowbuilder', '--configfile', 
         'xenial-cowbuilder', '--build', util.Property('dsc_path')], description="Build Deb"))
 
-def aptly(name, factory):
-    aptly_url = 'https://' + creds.aptly + '@aptly.ubiquityrobotics.com/'
-
-    factory.addStep(steps.ShellSequence(
-        commands = [
-            util.ShellArg(command=['curl', '-X', 'POST', '-F', util.Interpolate('file=@%(prop:deb_path)s'), 
-                aptly_url + 'api/files/' + name
-            ]),
-
-            util.ShellArg(command=['curl', '-X', 'POST', aptly_url + 'api/repos/main-building/file/' + name]),
-
-            util.ShellArg(command=['curl', '-X', 'PUT', aptly_url + 'api/publish/filesystem:www:building/xenial',
-                '-H', "Content-Type: application/json", '--data', "{}"
-            ])
-        ],
-        description='Push deb to building',
-        descriptionDone='Pushed deb to building'
-    ))
-    factory.addStep(steps.ShellSequence(
-        commands = [
-            util.ShellArg(command=['curl', '-X', 'POST', '-F', util.Interpolate('file=@%(prop:deb_path)s'), 
-                aptly_url + 'api/files/' + name
-            ]),
-
-            util.ShellArg(command=['curl', '-X', 'POST', aptly_url + 'api/repos/main-testing/file/' + name]),
-
-            util.ShellArg(command=['curl', '-X', 'PUT', aptly_url + 'api/publish/filesystem:www:ubiquity-testing/xenial',
-            '-H', "Content-Type: application/json", '--data', "{}"
-            ])
-        ],
-        description='Push deb to testing',
-        descriptionDone='Pushed deb to testing'
-    ))
-
 def single_ros_deb(name, release_repo, arch, factory):
     factory.addStep(steps.Git(repourl=release_repo,
                                      branch='debian/kinetic/xenial/%s' % name, method='clobber', mode='full', alwaysUseLatest=True))
@@ -129,3 +98,36 @@ def metapackage_ros_backport_deb(name, packages, release_repo, arch, factory):
     # TODO Topological sort, right now we assume that the packages are already in sorted order
     for package in packages:
         single_ros_backport_deb(package, release_repo, arch, factory)
+
+def aptly(name, factory):
+    aptly_url = 'https://' + creds.aptly + '@aptly.ubiquityrobotics.com/'
+
+    factory.addStep(steps.ShellSequence(
+        commands = [
+            util.ShellArg(command=['curl', '-X', 'POST', '-F', util.Interpolate('file=@%(prop:deb_path)s'), 
+                aptly_url + 'api/files/' + name
+            ]),
+
+            util.ShellArg(command=['curl', '-X', 'POST', aptly_url + 'api/repos/main-building/file/' + name]),
+
+            util.ShellArg(command=['curl', '-X', 'PUT', aptly_url + 'api/publish/filesystem:www:building/xenial',
+                '-H', "Content-Type: application/json", '--data', "{}"
+            ])
+        ],
+        description='Push deb to building',
+        descriptionDone='Pushed deb to building'
+    ))
+
+    query = util.Interpolate('Name (=%(prop:deb_package_name)s), Version (=%(prop:deb_version)s)')
+    factory.addStep(aptly_steps.AptlyCopyPackageStep(aptly_url, 'main-building', 'main-testing', query))
+
+    factory.addStep(steps.ShellSequence(
+        commands = [
+            util.ShellArg(command=['curl', '-X', 'PUT', aptly_url + 'api/publish/filesystem:www:ubiquity-testing/xenial',
+            '-H', "Content-Type: application/json", '--data', "{}"
+            ])
+        ],
+        description='Push deb to testing',
+        descriptionDone='Pushed deb to testing'
+    ))
+
