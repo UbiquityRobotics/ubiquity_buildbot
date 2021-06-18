e5153a5e158009180d94641d4f91f3a802b5c62f
Rohan Agrawal <send2arohan@gmail.com>
2021-06-18 14:48:17 -0700

    Add support for noetic builds, and check repository signatures

--- ubiquity_buildsteps.py
+++ ubiquity_buildsteps.py
@@ -6,11 +6,11 @@ from buildbot.plugins import *
 from get_creds import creds
 import aptly_steps
 
-def set_deb_properties(name, factory, arch, is_ros=False):
+def set_deb_properties(name, distro, factory, arch, is_ros=False, ros_distro=""):
     '''
     This functions sets the dsc_path and deb_path buildbot properties for the build
 
-    For ROS packages this includes prepending 'ros-DISTRO'
+    For ROS packages this includes prepending 'ros-{ros_distro}'
     '''
     factory.addStep(steps.SetPropertyFromCommand(command="dpkg-parsechangelog --show-field Version", 
         property="deb_version", haltOnFailure=True))
@@ -20,15 +20,15 @@ def set_deb_properties(name, factory, arch, is_ros=False):
 
     if is_ros:
         factory.addStep(steps.SetProperty(property="deb_path", 
-            value=util.Interpolate("/var/cache/pbuilder/result/xenial/ros-kinetic-" + debian_name + "_%(prop:deb_version)s_" + arch +".deb")))
+            value=util.Interpolate(f"/var/cache/pbuilder/result/{distro}/ros-{ros_distro}-{debian_name}_%(prop:deb_version)s_{arch}.deb")))
         factory.addStep(steps.SetProperty(property="dsc_path", 
-            value=util.Interpolate("../ros-kinetic-" + debian_name + "_%(prop:deb_version)s.dsc")))
-        factory.addStep(steps.SetProperty(property="deb_package_name", value='ros-kinetic-' + debian_name))
+            value=util.Interpolate(f"../ros-{ros_distro}-{debian_name}_%(prop:deb_version)s.dsc")))
+        factory.addStep(steps.SetProperty(property="deb_package_name", value=f'ros-{ros_distro}-{debian_name}'))
     else:
         factory.addStep(steps.SetProperty(property="deb_path", 
-            value=util.Interpolate("/var/cache/pbuilder/result/xenial/" + debian_name + "_%(prop:deb_version)s_" + arch +".deb")))  
+            value=util.Interpolate(f"/var/cache/pbuilder/result/{distro}/{debian_name}_%(prop:deb_version)s_{arch}.deb")))  
         factory.addStep(steps.SetProperty(property="dsc_path", 
-            value=util.Interpolate("../" + debian_name + "_%(prop:deb_version)s.dsc")))
+            value=util.Interpolate(f"../{debian_name}_%(prop:deb_version)s.dsc")))
         factory.addStep(steps.SetProperty(property="deb_package_name", value=debian_name))
 
 def cowbuilder_test_path(cow_config):
@@ -43,65 +43,77 @@ def cowbuilder_test_path(cow_config):
             if match is not None:
                 result[match.group(1)] = match.group(2)
 
+    # Return a path to a file that would exist if the cowbuilder was already created
     return result['BASEPATH'] + '/etc/os-release'
 
-def cowbuilder(arch, factory):
+def cowbuilder(distro, arch, factory):
     '''
     Uses cowbuilder to build a dsc file into debs.
 
     The dsc_path property must be set
     '''
 
-    cow_config = "cowbuilder/xenial-cowbuilder-%s" % arch
+    cow_config = f"cowbuilder/{distro}-cowbuilder-{arch}"
     cow_test = cowbuilder_test_path(cow_config)
 
-    factory.addStep(steps.FileDownload(mastersrc=cow_config, workerdest="xenial-cowbuilder"))
+    factory.addStep(steps.FileDownload(mastersrc=cow_config, workerdest=f"{distro}-cowbuilder-{arch}"))
+    factory.addStep(steps.FileDownload(mastersrc="cowbuilder/ros-archive-keyring.gpg", workerdest="ros-archive-keyring.gpg"))
+    factory.addStep(steps.FileDownload(mastersrc="cowbuilder/ubiquity-archive-keyring.gpg", workerdest="ubiquity-archive-keyring.gpg"))
     
     # Short shell invocation to create the cowbuilder if it doesn't exist already
     factory.addStep(steps.ShellCommand(
-        command='[ -f {} ] || sudo cowbuilder --create --config {}'.format(cow_test, 'xenial-cowbuilder'),
+        command=f"[ -f {cow_test} ] || sudo cowbuilder --create --config {distro}-cowbuilder-{arch}",
         description="Create Cowbuilder"
     ))
 
     factory.addStep(steps.ShellCommand(command=['sudo', 'cowbuilder', '--configfile', 
-        'xenial-cowbuilder', '--update'], description="Update Cowbuilder"))
+        f'{distro}-cowbuilder-{arch}', '--update'], description="Update Cowbuilder"))
     factory.addStep(steps.ShellCommand(command=['sudo', 'cowbuilder', '--configfile', 
-        'xenial-cowbuilder', '--build', util.Property('dsc_path')], description="Build Deb"))
+        f'{distro}-cowbuilder-{arch}', '--build', util.Property('dsc_path')], description="Build Deb"))
 
-def single_ros_deb(name, release_repo, arch, factory):
+def single_ros_deb(name, release_repo, distro, ros_distro, arch, factory):
     factory.addStep(steps.Git(repourl=release_repo,
-                                     branch='debian/kinetic/xenial/%s' % name, method='clobber', mode='full', alwaysUseLatest=True))
+                                     branch=f'debian/{ros_distro}/{distro}/{name}', method='clobber', mode='full', alwaysUseLatest=True))
     factory.addStep(steps.ShellCommand(command=['gbp', 'buildpackage', '-S', '-us', '-uc', '-d', '', '--git-ignore-new', '--git-ignore-branch'],
                                               description="Generate Source Deb", haltOnFailure=True))
-    set_deb_properties(name, factory, arch, is_ros=True)
-    cowbuilder(arch, factory)
-    aptly(name, factory)
+    set_deb_properties(name, distro, factory, arch, is_ros=True, ros_distro=ros_distro)
+    cowbuilder(distro, arch, factory)
+    aptly(distro, name, factory)
 
 
-def single_ros_backport_deb(name, release_repo, arch, factory):
+def single_ros_backport_deb(name, release_repo, distro, ros_distro, arch, factory):
     factory.addStep(steps.Git(repourl=release_repo,
-                                     branch='debian/kinetic/xenial/%s' % name, method='clobber', mode='full', alwaysUseLatest=True))
-    factory.addStep(steps.ShellCommand(command=['dch','--local','~tbpo','--distribution','xenial',"Temp Backport"],
+                                     branch=f'debian/{ros_distro}/{distro}/{name}', method='clobber', mode='full', alwaysUseLatest=True))
+    factory.addStep(steps.ShellCommand(command=['dch','--local','~tbpo','--distribution', distro,"Temp Backport"],
                                               description="Bump to backport version", haltOnFailure=True))
     factory.addStep(steps.ShellCommand(command=['gbp', 'buildpackage', '-S', '-us', '-uc', '-d', '', '--git-ignore-new', '--git-ignore-branch'],
                                               description="Generate Source Deb", haltOnFailure=True))
-    set_deb_properties(name, factory, arch, is_ros=True)
-    cowbuilder(arch, factory)
-    aptly(name, factory)
+    set_deb_properties(name, distro, factory, arch, is_ros=True, ros_distro=ros_distro)
+    cowbuilder(distro, arch, factory)
+    aptly(distro, name, factory)
 
-def metapackage_ros_deb(name, packages, release_repo, arch, factory):
+def metapackage_ros_deb(name, packages, release_repo, distro, ros_distro, arch, factory):
     # TODO Topological sort, right now we assume that the packages are already in sorted order
     for package in packages:
-        single_ros_deb(package, release_repo, arch, factory)
+        single_ros_deb(package, release_repo, distro, ros_distro, arch, factory)
 
-def metapackage_ros_backport_deb(name, packages, release_repo, arch, factory):
+def metapackage_ros_backport_deb(name, packages, release_repo, distro, ros_distro, arch, factory):
     # TODO Topological sort, right now we assume that the packages are already in sorted order
     for package in packages:
-        single_ros_backport_deb(package, release_repo, arch, factory)
+        single_ros_backport_deb(package, release_repo, distro, ros_distro, arch, factory)
 
-def aptly(name, factory):
+def aptly(distro, name, factory):
     aptly_url = 'https://aptly.ubiquityrobotics.com/'
     aptly_url_creds = 'https://' + creds.aptly[0] + ':' + creds.aptly[1] + '@aptly.ubiquityrobotics.com/'
+
+    building_repo = f"{distro}-main-building"
+    testing_repo = f"{distro}-main-testing"
+
+    # Due to a lack of foresight the xenial repos don't have the distro name
+    if distro == "xenial":
+        building_repo = "main-building"
+        testing_repo = "main-testing"
+        
 
     factory.addStep(steps.ShellSequence(
         commands = [
@@ -109,7 +121,7 @@ def aptly(name, factory):
                 aptly_url_creds + 'api/files/' + name
             ]),
 
-            util.ShellArg(command=['curl', '-X', 'POST', aptly_url_creds + 'api/repos/main-building/file/' + name])
+            util.ShellArg(command=['curl', '-X', 'POST', aptly_url_creds + f'api/repos/{building_repo}/file/' + name])
         ],
         description='Push deb to building',
         descriptionDone='Pushed deb to building'
@@ -117,17 +129,17 @@ def aptly(name, factory):
     
     factory.addStep(aptly_steps.AptlyUpdatePublishStep(
         aptly_url, creds.aptly,
-        'filesystem:www:building/xenial'
+        f'filesystem:www:building/{distro}'
     ))
 
     factory.addStep(aptly_steps.AptlyCopyPackageStep(
         aptly_url, creds.aptly,
-        'main-building', 'main-testing',
+        building_repo, testing_repo,
         util.Interpolate('Name (=%(prop:deb_package_name)s), Version (=%(prop:deb_version)s)')
     ))
 
     factory.addStep(aptly_steps.AptlyUpdatePublishStep(
         aptly_url, creds.aptly,
-        'filesystem:www:ubiquity-testing/xenial'
+        f'filesystem:www:ubiquity-testing/{distro}'
     ))
 
