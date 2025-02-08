587c5e86a9c9bf6467ca32d7c130f801fccb9040
Timotej Gregorič <35196879+GreTimotej@users.noreply.github.com>
2025-02-08 09:39:31 +0100

    Update customize_ezmap_lps.py

--- customizations/customize_ezmap_lps.py
+++ customizations/customize_ezmap_lps.py
@@ -74,8 +74,9 @@ class customizeImage:
 	def execute_customizations(self):
 		#gdm3_mod.install()
 
-		#disable GUI
+		#disable GUI and timeout
 		subprocess.run("systemctl set-default multi-user.target", shell=True, check=True, executable='/bin/bash')
+		subprocess.run("gsettings set org.gnome.desktop.session idle-delay 0", shell=True, check=True, executable='/bin/bash')
 
 		subprocess.run("git config --global credential.helper 'cache --timeout=120'", shell=True, check=True, executable='/bin/bash')
 
