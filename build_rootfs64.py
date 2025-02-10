616f9ae0c37c042122a4c31c8be3d0de3cedb0ed
Luka Kidric <luka.kidric.24@gmail.com>
2025-02-10 16:31:10 +0100

    Update build_rootfs64.py

--- build_rootfs64.py
+++ build_rootfs64.py
@@ -238,7 +238,7 @@ def common_ubiquity_customizations(release="noble",
             #"raspberrypi-bootloader",
             "linux-firmware",
             "raspi-config",
-            "pigpiod",
+            #"pigpiod",
             # Network Access
             "openssh-server",
             "libnss-mdns",
@@ -494,6 +494,29 @@ def common_ubiquity_customizations(release="noble",
     #shutil.copy("/files/pifi.conf", "/etc/pifi/pifi.conf")
     #shutil.copy("/files/default_ap.em", "/etc/pifi/default_ap.em")
     subprocess.run(["echo", "//////////////////////////////BREAK//////////////////////"])
+    # Install dependencies
+    subprocess.run(["apt", "install", "-y", "python3-pigpio", "python3-pip"], check=True)
+    
+    # Clone the pigpio repository
+    subprocess.run(["git", "clone", "https://github.com/joan2937/pigpio.git", "/tmp/pigpio"], check=True)
+    
+    # Change to the pigpio directory
+    os.chdir("/tmp/pigpio")
+    
+    # Build pigpio
+    subprocess.run(["make"], check=True)
+    
+    # Install pigpio
+    subprocess.run(["make", "install"], check=True)
+    
+    # Start the pigpio daemon
+    subprocess.run(["pigpiod"], check=True)
+    
+    # Enable the pigpio daemon to start on boot
+    subprocess.run(["systemctl", "enable", "pigpiod"], check=True)
+    
+    # Install the Python library
+    subprocess.run(["pip3", "install", "pigpio"], check=True)
     # Enable pigpio daemon that we use for sonars
     subprocess.run(["systemctl", "enable", "pigpiod.service"], check=True)
     subprocess.run(["echo", "//////////////////////////////BREAK//////////////////////"])
